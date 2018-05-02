d4e545e00d6917e53c12e33100be5fea77fab22d
Colin Farrell <colinpfarrell@gmail.com>
2018-05-01 21:11:28 -0700

    LassoCV

---
+++ LaunchLassoCV.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from LassoCVInterface import ParseClassInput
+import argparse
+
+parser = argparse.ArgumentParser(description='Interface for parallel SGE implementation of SciKitLearn ElasticNetCV')
+parser.add_argument('-i', type=str)
+
+arguments = parser.parse_args()
+
+
+ParseClassInput(input_dictionary=arguments.i)
